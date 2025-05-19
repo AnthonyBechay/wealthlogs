aeb04243e722b7d9fe1b9abff4d3f14f6db4c39d
WilliamBechay <williambechay23123@gmail.com>
2025-05-19 13:28:20 -0400

    dwda

--- wealthlog/apps/web/pages/accounts.tsx
+++ wealthlog/apps/web/pages/accounts.tsx
@@ -1,4 +1,4 @@
-// test9000 pages/accounts.tsx
+// test90000 pages/accounts.tsx
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { api } from "@wealthlog/common"; // your shared axios instance
