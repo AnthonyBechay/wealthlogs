bcd20eca167c93cd19f2875dbf6317998c21e662
WilliamBechay <williambechay23123@gmail.com>
2025-05-19 13:30:54 -0400

    Test

--- wealthlog/apps/web/pages/accounts.tsx
+++ wealthlog/apps/web/pages/accounts.tsx
@@ -1,7 +1,8 @@
-// test90000 pages/accounts.tsx
+//pages/accounts.tsx
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { api } from "@wealthlog/common"; // your shared axios instance
+//interface
 
 interface FinancialAccount {
   id: number;
