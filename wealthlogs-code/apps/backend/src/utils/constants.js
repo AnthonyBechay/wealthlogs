d91b9e56cd1c8f8b009df88eb3737a9454a8f772
anthonybechay <anthonybechay1@gmail.com>
2025-08-21 18:05:21 +0300

    fixing authentication

--- wealthlogs-code/apps/backend/src/utils/constants.js
+++ wealthlogs-code/apps/backend/src/utils/constants.js
@@ -11,7 +11,7 @@ module.exports = {
   COOKIE_OPTIONS: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // 'none' required for cross-domain cookies
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: '/'
   },
